81f4eb3ce63cefd44542a093648582e38cc3e81a
Josh <j-kozlowski@live.com>
2015-07-13 19:37:03 -0400

    made changes to settings.py

--- landing/settings.py
+++ landing/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'rrgq4!mzhqzl$z_-fa%z=c9nar1o1qxc%z3!3g59g9m%&(f!ec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
