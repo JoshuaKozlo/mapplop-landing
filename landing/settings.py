12f7388421174669d25f7ec68090fbd3f5d19c90
Josh <j-kozlowski@live.com>
2015-07-13 00:26:00 -0400

    Dropped STATICFILE_DIRS down one directory

--- landing/settings.py
+++ landing/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS =(
-     os.path.join(BASE_DIR, 'static', "our_static"),
+     os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
