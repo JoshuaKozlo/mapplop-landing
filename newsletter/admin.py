e6f3a5ef3876296544e31fd63c978fcdd14d0c5c
Josh <j-kozlowski@live.com>
2015-07-12 20:37:06 -0400

    Added jingle, changed timezones, and made all fields visable in admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -5,8 +5,13 @@ from django.contrib import admin
 from .models import SignUp
 
 
+class SignUpAdmin(admin.ModelAdmin):
+	list_display = ('email', 'full_name', 'ip', 'user_type', 'timestamp')
+	class Meta:
+		model = SignUp
+	
 
 
 
 
-admin.site.register(SignUp)
+admin.site.register(SignUp, SignUpAdmin)
