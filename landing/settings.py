ab29335cde04ed63d2e62a2d762f10b0e83cbb43
Josh <j-kozlowski@live.com>
2015-07-14 21:15:21 -0400

    Made changes to settings.py

--- landing/settings.py
+++ landing/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import psycopg2
-import urlparse
+import urlparse.parse
 import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
